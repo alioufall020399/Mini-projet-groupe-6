715599111a70b97944a72a98c4fe0a99bd49f7fe
alioufall020399 <alioufall020399@gmail.com>
2024-12-20 10:57:13 +0100

    Update calculatrice.C

---
+++ Calculatrice.C
@@ -0,0 +1,13 @@
+include<stdio.h>
+include<string.h>
+
+//Declaration de variables
+
+//Fonction Addition ALIOU
+int somme (int a, int b) {
+return a+b;
+}
+int somme(float a, int b) 
+//Fonction Multiplication RYAD
+
+//Fonction Division NDIAYA
